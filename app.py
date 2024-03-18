e390fe77ed3d23d1cae810500f79c4d2ebcf634d
Aswathi <akv2595@gmail.com>
2024-03-18 13:34:55 -0400

    adding a new template

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from markupsafe import escape
-from flask import Flask, abort
+import datetime
+from flask import Flask, abort, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 @app.route('/about/')
 def about():
-    return '<h3>This is a Flask web application.</h3>'
+    return render_template('about.html')
 
 @app.route('/capitalize/<word>/')
 def capitalize(word):
